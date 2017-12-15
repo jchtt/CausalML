097fbb4d25eec601bbab141d3ee6b2e8e3ab7752
jchtt <jchtt@arcor.de>
2017-12-14 23:35:26 -0500

    Update

--- runscript.py
+++ runscript.py
@@ -4,7 +4,7 @@ import os
 import datetime
 import time
 
-task = "semi_synth_vare"
+tasks = [("semi_synth_vare", 1)]
 last_start = 14
 last_minute = 5
 cur_time = datetime.datetime.now()
@@ -14,12 +14,22 @@ cut_off = cut_off.replace(hour = last_start, minute = last_minute, second = 0)
 # cut_off = cur_time.replace(minute = 57)
 
 start_index = 1
-end_index = 1
+task_index = 0
+task = tasks[task_index][0]
+end_index = tasks[task_index][1]
 
-while cur_time < cut_off and start_index <= end_index:
+while cur_time < cut_off and start_index <= end_index and task_index < length(tasks):
     os.system("julia5 CausalMLTest.jl {} {} &> ./logs/log_{}.txt".format(start_index, task, start_index))
     # os.system("touch test/{}".format(start_index))
     time.sleep(1)
 
     start_index += 1
+    if start_index > end_index:
+        if task_index < length(tasks) - 1:
+            task_index += 1
+            task = tasks[task_index][0]
+            end_index = tasks[task_index][1]
+            start_index = 1
+        else:
+            break
     cur_time = datetime.datetime.now()
